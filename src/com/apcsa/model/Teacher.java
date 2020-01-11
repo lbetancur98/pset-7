bcb4595be3931bae66f314b4d96e993759c127f9
Lukas Betancur <betancurlukas98@gmail.com>
2020-01-11 14:03:01 -0500

    30

--- src/com/apcsa/model/Teacher.java
+++ src/com/apcsa/model/Teacher.java
@@ -1,6 +1,12 @@
 package com.apcsa.model;
 
 import java.sql.ResultSet;
+import java.sql.*;
+import java.util.*;
+
+import com.apcsa.model.*;
+import com.apcsa.data.*;
+import com.apcsa.controller.*;
 import java.sql.SQLException;
 
 import com.apcsa.model.User;
@@ -11,6 +17,7 @@ public class Teacher extends User {
     private int departmentId;
     private String firstName;
     private String lastName;
+    private String departmentName;
     
     public Teacher(User user, ResultSet rs) throws SQLException {
 		super(user.getUserId(), user.getAccountType(), user.getUsername(), user.getPassword(), user.getLastLogin());
@@ -21,6 +28,23 @@ public class Teacher extends User {
 		this.lastName = rs.getString("last_name");
 		this.departmentId = rs.getInt("department_id");
 	}
+    
+    public Teacher(ResultSet rs) throws SQLException {
+		super(rs.getInt("user_id"), rs.getString("account_type"), rs.getString("username"), rs.getString("auth"), rs.getString("last_login"));
+		this.teacherId = rs.getInt("teacher_id");
+		this.firstName = rs.getString("first_name");
+		this.lastName = rs.getString("last_name");
+        this.departmentName = rs.getString("title");
+        this.departmentId = rs.getInt("department_id");
+	}
+    
+    public String getDepartmentName() {
+        return this.departmentName;
+    }
+    
+    public int getDepartmentId() {
+        return departmentId;
+    }
 
     public int getTeacherId() {
     	return teacherId;
@@ -34,7 +58,54 @@ public class Teacher extends User {
     	return lastName;
     }
     
-    public int getDepartmentId() {
-    	return departmentId;
-    }
+    
+    
+ public void enrollment(Scanner in) {
+		
+        int input = 0;
+        boolean assignments = false;
+        ArrayList<String> course_nos = getTeacherCourseList();
+
+		try {
+			input = in.nextInt();
+		} catch (InputMismatchException e) {
+			System.out.println("\nYour input was invalid. Please try again.");
+		} finally {
+			in.nextLine();
+        }
+
+        System.out.print("\n");
+        try (Connection conn = PowerSchool.getConnection()) {
+             PreparedStatement stmt = conn.prepareStatement(QueryUtils.GET_STUDENT_ENROLLMENT_BY_COURSE_NO);
+             stmt.setString(1, course_nos.get(input - 1));
+             try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    //Checks to see if assignments exist, as if they do, the grade must be shown.
+                    //If they don't exist, just "--" is put in place of the grade. 
+                    try (Connection conn2 = PowerSchool.getConnection()) {
+                        PreparedStatement stmt2 = conn2.prepareStatement("SELECT * FROM assignment_grades WHERE student_id = ?");
+                        stmt2.setInt(1, rs.getInt("STUDENT_ID"));
+                        try (ResultSet rs2 = stmt2.executeQuery()) {
+                            if (rs2.next()) {
+                                assignments = true;
+                            }
+                        }
+                    }
+                    
+                    if (assignments) {
+                        System.out.println(rs.getString("last_name") + ", " + rs.getString("first_name") + " / " + rs.getInt("grade"));
+                    } else {
+                        System.out.println(rs.getString("last_name") + ", " + rs.getString("first_name") + " / " + "--");
+                    }
+
+                assignments = false;
+                }
+             }
+        } catch (SQLException e) {
+            PowerSchool.shutdown(true);
+        }
+        
+        
+     }
+
 }
