1d3b4baec66616ab3f321b9db3f416b601fa06c8
Lukas Betancur <betancurlukas98@gmail.com>
2020-01-11 13:58:03 -0500

    27

--- src/com/apcsa/model/Student.java
+++ src/com/apcsa/model/Student.java
@@ -1,6 +1,18 @@
 package com.apcsa.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+import com.apcsa.controller.Utils;
+import com.apcsa.data.*;
+import com.apcsa.model.User;
 import java.sql.SQLException;
 
 import com.apcsa.model.User;
@@ -28,6 +40,19 @@ public class Student extends User {
 		this.gpa = rs.getDouble("gpa");
 	}
     
+    public Student(ResultSet rs) throws SQLException {
+		//user id, account type, username, password, last login
+		super(rs.getInt("user_id"), rs.getString("account_type"), rs.getString("username"), rs.getString("auth"), rs.getString("last_login"));
+
+		this.studentId = rs.getInt("student_id");
+    	this.classRank = rs.getInt("class_rank");
+    	this.gradeLevel = rs.getInt("grade_level");
+    	this.graduationYear = rs.getInt("graduation");
+    	this.gpa = rs.getDouble("gpa");
+    	this.firstName = rs.getString("first_name");
+		this.lastName = rs.getString("last_name");
+	}
+    
     public int getStudentId() {
     	return studentId;
     }
@@ -43,6 +68,10 @@ public class Student extends User {
     public int getClassRank() {
     	return classRank;
     }
+    
+    public void setClassRank(int classRank) {
+		this.classRank = classRank;
+	}
     
     public int getGradeLevel() {
     	return gradeLevel;
